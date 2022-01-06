a22e9f618b635469218b6d04e77ff56397029833
Michael <michael@unetresgrossebite.com>
2022-01-06 13:54:51 +0100

    update medecine skill

--- scripts/pnjs/components/skillTarget.mjs
+++ scripts/pnjs/components/skillTarget.mjs
@@ -2,34 +2,48 @@ import {DialogComponent} from "./dialog.mjs";
 import {getValueFromRange} from "../../behaviour/random.mjs";
 import {stealCheck} from "../../cmap/helpers/checks.mjs";
 
+const medecineInterval    = 21600000;
+const medecineMaxUseCount = 2;
+
 export class SkillTargetComponent extends DialogComponent {
   constructor(model) {
     super(model);
   }
 
   onUseMedicine(user) {
+    const useCount = this.model.hasVariable("medecineUses") ? this.model.getVariable("medecineUses") : 0;
+
     if (!this.model.isAlive())
       game.appendToConsole(i18n.t("messages.invalid-target"));
     else if (level.isInCombat(user))
       game.appendToConsole(i18n.t("medicine-skill.in-combat"));
-    else
-    {
+    else if (useCount + 1 > medecineMaxUseCount)
+      game.appendToConsole(i18n.t("medecine-skill.max-use-reached"));
+    else {
       const stats   = this.model.statistics;
       const maxHeal = stats.maxHitPoints - stats.hitPoints;
       var   healed  = getValueFromRange(0, 10) + user.statistics.medicine / 10;
- 
+
       healed = healed * user.statistics.medicine / 100;
       healed = Math.min(healed, maxHeal);
+      healed = Math.ceil(healed);
       stats.hitPoints += healed;
       game.appendToConsole(i18n.t("medicine-skill.used-on", {
         user:   user.statistics.name,
         target: stats.name,
         hp:     healed
       }));
-      level.addTextBubble(this.model, "Thank you kindly !", 5000, "lightgreen");
+      this.model.setVariable("medecineUses", useCount + 1);
+      this.model.tasks.addTask("medecineSkillWearOff", medecineInterval);
       return true;
     }
     return false;
+  }
+
+  medecineSkillWearOff() {
+    const useCount = this.model.getVariable("medecineUses");
+    if (useCount > 1) { this.model.setVariable("medecineUses", useCount - 1); }
+    else              { this.model.unsetVariable("medecineUses"); }
   }
 
   onTakeItem(user, item, quantity) {
